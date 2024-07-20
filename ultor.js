0ccc2fae33c318be73a0da61efac311cb2f9d49c
Aliyan Ahmed <MY_NAME@example.com>
2024-07-20 11:37:33 -0700

    meteor thingy

---
+++ ultor.js
@@ -0,0 +1,32 @@
+    // // let input = prompt("enter secs")
+    // let input = 1;
+    // if(input >= ((12*60)*60))
+    // {
+    //     // input = input - ((24*60)*60)
+    //     let min = Math.floor(input / (60 * 24))
+    //     min =0
+    //     let remainder = input % 60
+    //     let hour = Math.floor((input / 60) / 60)
+    //     if(hour >= 12)
+    //     {
+    //         hour = hour - 12
+    //         console.log(`${hour}:${min}:${remainder} PM`);        
+    //     }else{
+    //         console.log(`${hour}:${min}:${remainder} AM`);        
+    //     }
+    // }else{
+    //         let min = Math.floor(input / (60 * 24))
+    //     let remainder = input % 60
+    //     let hour = Math.floor((input / 60) / 60)
+    //     if(hour >= 12)
+    //     {
+    //         hour = hour - 12
+    //         console.log(`${hour}:${min}:${remainder} PM`);        
+    //     }else{
+    //         console.log(`${hour}:${min}:${remainder} AM`);        
+    //     }
+    // }
+    
+function secondToTime(seconds){
+    let latation = (seconds %) 
+}
